753f987b2ec4de2a462e07d809df7dfc2ca08a02
Luis <luisprooc@gmail.com>
2021-02-16 10:01:03 -0400

    refactoring, manage correct answer and give feedback at the user

--- src/components/progress.js
+++ src/components/progress.js
@@ -1,18 +1,51 @@
 import React,{useEffect,useState} from 'react';
 
-const Progress = ({setCount,setTimer}) => {
+const Progress = ({setCount,setReplied,replied}) => {
     let [time,setTime] = useState(100);
 
-    useEffect(()=>{
+
+    useEffect(() => {
+        timer();
+
+    },[time]);
+
+    const timer = () => {
+
         setTimeout(()=>{
-            
-            setTime(time-=0.9);
+
+            if(time > 0){
+
+                // rest time
+                setTime(time-=0.9);
+                
+            }
+
+            // wait 500 miliseconds and render correct answer
+            else{
+                setTimeout(()=>{
+                    setCount(true);
+                    setReplied(replied + 1);
+
+                },500);
+
+                // Then render replieds and hidden correct answer
+
+                setTimeout(()=>{
+                    setCount(false);
+
+
+                },5000);
+
+                return;
+            }
+
         },90);
-    });
+
+    };
 
     return(
             <div className="progress">
-                <div className="progress-bar progress-bar-striped bg-success" role="progressbar" style={{width: `${time}%`}} aria-valuenow={time} aria-valuemin="0" aria-valuemax="100"></div>
+                <div className="progress-bar  bg-success" role="progressbar" style={{width: `${time}%`}} aria-valuenow={time} aria-valuemin="0" aria-valuemax="100"></div>
             </div>
     );
 };
--- src/components/random-quiz.js
+++ src/components/random-quiz.js
@@ -6,6 +6,8 @@ const RandomQuiz = () => {
 
     const [count,setCount] = useState(false);
     const [quiz,saveQuiz] = useState({});
+    const [corrects,setCorrects] = useState(0);
+    const [replied,setReplied] = useState(0);
 
     useEffect(()=>{
 
@@ -14,49 +16,52 @@ const RandomQuiz = () => {
             .then(res => saveQuiz(res))
             .catch(error => console.log(error))
 
-    },[count]);
+    },[]);
 
-    const nextQuestion = e => {
-        if(e.target.classList.contains("disabled")) return;
-        
-        e.target.classList.add("disabled");
-        setCount(true);
-    };
-    
-
-    return(
+    // If quiz has id render it
+    return quiz.id?(
         <div className="container py-5">
-            <h1>RANDOM QUIZ</h1>
-            <Progress setCount={setCount} />
+            <h2 className="text-center">{quiz.question}</h2>
+
+            <Progress 
+                setCount={setCount} 
+                setReplied={setReplied}
+                replied={replied}
+            />
+
+            {/*If count is true, render correct answer otherwise show replieds */}
+
             {count?(
                 <div className="alert alert-dismissible alert-secondary">
                     <strong>Correct Answer: </strong> {quiz.correct}
                 </div>
-            ):null}
-
-            { quiz.id && (
-                <div className="mt-4">
-                    <h3 className="text-center">{quiz.question}</h3>
-                    <div className="py-4 d-flex flex-column align-items-center">
-                        {quiz.answers.sort((a,b) => a.optId - b.optId).map(item =>{
-                            const {opt,optId} = item;
-                            return(
-                                <div key={optId}>
-                                    <p>{opt}</p>
-                                    
-                                </div>
-                            )
-                        })}
-                    </div>
+            ):(
+                <div className="alert alert-dismissible alert-light">
+                    <strong>Corrects: {corrects} -- Replied: {replied}</strong>
                 </div>
-
             )}
+
+            <div className="mt-4">
+                <div className="py-2 d-flex flex-column align-items-center">
+
+                    {/*Display answers */}
+
+                    {quiz.answers.sort((a,b) => a.optId - b.optId).map(item =>{
+                        const {opt,optId} = item;
+                        return(
+                            <button key={optId} value={opt} className="btn btn-info" style={{width:"70%", margin:"1rem auto"}}>
+                                {opt}
+                            </button>
+                        )
+                    })}
+                </div>
+            </div>
+
             <div className="d-flex mt-4 justify-content-between">
-                <span className="icon">{quiz.Category === "Ciencia"?'🔬':'👩‍💻'}</span>
-                <button type="button" className={`btn btn-success ${count?"":"disabled"} btn-lg`}  onClick={(e)=> nextQuestion(e)}>Next Question</button>
+                <span className="icon" role="icon">Category: {quiz.category === "Ciencia"?'🔬':'👩‍💻'}</span>
             </div>
         </div>
-    );
+    ):null;
 };
 
 export default RandomQuiz;
